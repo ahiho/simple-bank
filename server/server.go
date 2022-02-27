031abbf9f0dd447701fc22b1cfe69817a572c92c
Duc Khue {duckhue01} <duckhuejs@gmail.com>
2022-02-27 13:13:30 +0700

    update: update ci/cd

--- server/server.go
+++ server/server.go
@@ -1,6 +1,9 @@
 package server
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/duckhue01/back-end/store/store"
 	"github.com/duckhue01/back-end/util"
 	"github.com/gin-gonic/gin"
@@ -27,13 +30,21 @@ func NewServer(conf *util.Config, store store.Store) (*Server, error) {
 
 func (server *Server) setupRouter() {
 	router := gin.Default()
+	router.GET("/", func(ctx *gin.Context) {
+		count := 1
+		for i := 0; i < 10000000000; i++ {
+			count = i
+		}
+		ctx.String(http.StatusOK, fmt.Sprint(count))
+
+	})
 
 	// router.POST("/users", server.createUser)
 	// router.POST("/users/login", server.loginUser)
 
-	router.POST("/accounts", server.createAccount)
-	router.GET("/accounts/:id", server.getAccount)
-	router.GET("/accounts", server.listAccounts)
+	// router.POST("/accounts", server.createAccount)
+	// router.GET("/accounts/:id", server.getAccount)
+	// router.GET("/accounts", server.listAccounts)
 
 	// router.POST("/transfers", server.createTransfer)
 
